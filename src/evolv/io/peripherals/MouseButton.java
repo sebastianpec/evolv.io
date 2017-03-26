7e289566db16cc3ac614998f4bde6d5bca693589
Nathan <n8style@yahoo.com>
2017-03-26 03:29:53 +0100

    tabs/spacing

--- src/evolv/io/peripherals/MouseButton.java
+++ src/evolv/io/peripherals/MouseButton.java
@@ -6,24 +6,24 @@ import java.util.Map;
 import processing.core.PConstants;
 
 public enum MouseButton {
-	NONE(0 /* no PConstants value for no mouse button */),
-	LEFT(PConstants.LEFT),
-	CENTER(PConstants.CENTER),
-	RIGHT(PConstants.RIGHT);
-	private static final Map<Integer, MouseButton>	CONSTANT_TO_MOUSE_BUTTONS	= new HashMap<>();
-	private final int											constant;
+   NONE(0 /* no PConstants value for no mouse button */),
+   LEFT(PConstants.LEFT),
+   CENTER(PConstants.CENTER),
+   RIGHT(PConstants.RIGHT);
+   private static final Map<Integer, MouseButton> CONSTANT_TO_MOUSE_BUTTONS = new HashMap<>();
+   private final int                              constant;
 
-	static {
-		for (MouseButton mouseButton : MouseButton.values()) {
-			CONSTANT_TO_MOUSE_BUTTONS.put(mouseButton.constant, mouseButton);
-		}
-	}
+   static {
+      for (MouseButton mouseButton : MouseButton.values()) {
+         CONSTANT_TO_MOUSE_BUTTONS.put(mouseButton.constant, mouseButton);
+      }
+   }
 
-	private MouseButton(int constant) {
-		this.constant = constant;
-	}
+   private MouseButton(int constant) {
+      this.constant = constant;
+   }
 
-	public static MouseButton getFromConstant(int constant) {
-		return CONSTANT_TO_MOUSE_BUTTONS.get(constant);
-	}
+   public static MouseButton getFromConstant(int constant) {
+      return CONSTANT_TO_MOUSE_BUTTONS.get(constant);
+   }
 }
