97e380df19a307b1b26e41623a478dc339242bdf
Nathan <n8style@yahoo.com>
2017-03-26 03:28:27 +0100

    tabs/spacing

--- src/evolv/io/peripherals/KeyAction.java
+++ src/evolv/io/peripherals/KeyAction.java
@@ -6,23 +6,23 @@ import java.util.Map;
 import processing.event.KeyEvent;
 
 public enum KeyAction {
-   PRESS(KeyEvent.PRESS),
-   RELEASE(KeyEvent.RELEASE),
-   TYPE(KeyEvent.TYPE);
-   private static final Map<Integer, KeyAction> CONSTANT_TO_KEY_ACTIONS = new HashMap<>();
-   private final int                            constant;
+	PRESS(KeyEvent.PRESS),
+	RELEASE(KeyEvent.RELEASE),
+	TYPE(KeyEvent.TYPE);
+	private static final Map<Integer, KeyAction>	CONSTANT_TO_KEY_ACTIONS	= new HashMap<>();
+	private final int										constant;
 
-   static {
-      for (KeyAction keyAction : KeyAction.values()) {
-         CONSTANT_TO_KEY_ACTIONS.put(keyAction.constant, keyAction);
-      }
-   }
+	static {
+		for (KeyAction keyAction : KeyAction.values()) {
+			CONSTANT_TO_KEY_ACTIONS.put(keyAction.constant, keyAction);
+		}
+	}
 
-   private KeyAction(int constant) {
-      this.constant = constant;
-   }
+	private KeyAction(int constant) {
+		this.constant = constant;
+	}
 
-   public static KeyAction getFromConstant(int constant) {
-      return CONSTANT_TO_KEY_ACTIONS.get(constant);
-   }
+	public static KeyAction getFromConstant(int constant) {
+		return CONSTANT_TO_KEY_ACTIONS.get(constant);
+	}
 }
--- src/evolv/io/peripherals/MouseAction.java
+++ src/evolv/io/peripherals/MouseAction.java
@@ -6,26 +6,26 @@ import java.util.Map;
 import processing.event.MouseEvent;
 
 public enum MouseAction {
-   PRESS(MouseEvent.PRESS),
-   RELEASE(MouseEvent.RELEASE),
-   CLICK(MouseEvent.CLICK),
-   WHEEL(MouseEvent.WHEEL),
-   MOVE(MouseEvent.MOVE),
-   DRAG(MouseEvent.DRAG);
-   private static final Map<Integer, MouseAction> CONSTANT_TO_KEY_ACTIONS = new HashMap<>();
-   private final int                              constant;
+	PRESS(MouseEvent.PRESS),
+	RELEASE(MouseEvent.RELEASE),
+	CLICK(MouseEvent.CLICK),
+	WHEEL(MouseEvent.WHEEL),
+	MOVE(MouseEvent.MOVE),
+	DRAG(MouseEvent.DRAG);
+	private static final Map<Integer, MouseAction>	CONSTANT_TO_KEY_ACTIONS	= new HashMap<>();
+	private final int											constant;
 
-   static {
-      for (MouseAction keyAction : MouseAction.values()) {
-         CONSTANT_TO_KEY_ACTIONS.put(keyAction.constant, keyAction);
-      }
-   }
+	static {
+		for (MouseAction keyAction : MouseAction.values()) {
+			CONSTANT_TO_KEY_ACTIONS.put(keyAction.constant, keyAction);
+		}
+	}
 
-   private MouseAction(int constant) {
-      this.constant = constant;
-   }
+	private MouseAction(int constant) {
+		this.constant = constant;
+	}
 
-   public static MouseAction getFromConstant(int constant) {
-      return CONSTANT_TO_KEY_ACTIONS.get(constant);
-   }
+	public static MouseAction getFromConstant(int constant) {
+		return CONSTANT_TO_KEY_ACTIONS.get(constant);
+	}
 }
